a857869a68d6d4ec489681765fd16b766538e109
Perets Viktoria <vktrprc@gmail.com>
2026-04-10 16:24:37 +0300

    feat: add database seeding

--- server/src/initDatabase.js
+++ server/src/initDatabase.js
@@ -5,11 +5,23 @@ export const initDatabase = async (req, res) => {
   try {
     const sqlQuery = await fs.readFile('./src/queries/createTables.sql', 'utf8');
 
-    db.exec(sqlQuery, (err) => {
+    db.exec(sqlQuery, async (err) => {
       if (err) {
         res.status(500).json({ error: err.message });
-      } else {
-        res.json({ message: "Усі таблиці успішно створено!" });
+      }
+      try {
+        const initData = await fs.readFile('./src/queries/completingTables.sql', 'utf8');
+
+        db.exec(initData, (err) => {
+          if (err) {
+            res.status(500).json({ error: err.message });
+          }
+
+          res.json({ message: "Таблиці успішно створено і заповнено даними!" });
+        });
+
+      } catch (error) {
+        res.status(500).json({ error: "Не вдалося прочитати файл з даними таблиць" });
       }
     });
   } catch (error) {
