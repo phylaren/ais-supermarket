8c2f2fb5bac7b6a7fb6ac796effcf25b62668a76
Perets Viktoria <vktrprc@gmail.com>
2026-04-10 15:09:19 +0300

    chore: correct foreign keys and adapt schema

--- server/src/queries/createTables.sql
+++ server/src/queries/createTables.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS Category (
-    category_number INTEGER NOT NULL PRIMARY KEY,
+    id_category INTEGER NOT NULL PRIMARY KEY,
     category_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Customer_Card (
-    card_number VARCHAR(13) NOT NULL PRIMARY KEY,
+    id_card VARCHAR(13) NOT NULL PRIMARY KEY,
     cust_surname VARCHAR(50) NOT NULL,
     cust_name VARCHAR(50) NOT NULL,
     cust_patronymic VARCHAR(50),
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS Customer_Card (
 
 CREATE TABLE IF NOT EXISTS Role (
     id_role INTEGER NOT NULL PRIMARY KEY,
-    role_name VARCHAR(50) NOT NULL UNIQUE
+    role_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Employee (
@@ -25,51 +25,51 @@ CREATE TABLE IF NOT EXISTS Employee (
     empl_surname VARCHAR(50) NOT NULL,
     empl_name VARCHAR(50) NOT NULL,
     empl_patronymic VARCHAR(50),
-    role_name VARCHAR(50) NOT NULL,
-    salary DECIMAL(13, 4) NOT NULL,
+    id_role INTEGER NOT NULL,
+    salary CURRENCY NOT NULL,
     date_of_birth DATE NOT NULL,
     date_of_start DATE NOT NULL,
     phone_number VARCHAR(13) NOT NULL,
     city VARCHAR(50) NOT NULL,
     street VARCHAR(50) NOT NULL,
     zip_code VARCHAR(9) NOT NULL,
-    FOREIGN KEY (role_name) REFERENCES Role (role_name) ON UPDATE CASCADE ON DELETE NO ACTION
+    FOREIGN KEY (id_role) REFERENCES Role (id_role) ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
 CREATE TABLE IF NOT EXISTS Product (
     id_product INTEGER NOT NULL PRIMARY KEY,
     product_name VARCHAR(50) NOT NULL,
     characteristics VARCHAR(100) NOT NULL,
-    category_number INT NOT NULL,
-    FOREIGN KEY (category_number) REFERENCES Category (category_number) ON DELETE NO ACTION ON UPDATE CASCADE
+    id_category INTEGER NOT NULL,
+    FOREIGN KEY (id_category) REFERENCES Category (id_category) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Store_Product(
     UPC VARCHAR(12) NOT NULL PRIMARY KEY,
-    selling_price DECIMAL(13, 4) NOT NULL,
-    products_number INT NOT NULL,
+    selling_price CURRENCY NOT NULL,
+    products_number INTEGER NOT NULL,
     promotional_product BIT NOT NULL,
-    id_product INT NOT NULL, 
+    id_product INTEGER NOT NULL, 
     FOREIGN KEY(id_product) REFERENCES Product(id_product) ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
 CREATE TABLE IF NOT EXISTS Receipt (
-    check_number VARCHAR(10) NOT NULL PRIMARY KEY,
+    id_check VARCHAR(10) NOT NULL PRIMARY KEY,
     print_date DATETIME NOT NULL,
-    sum_total DECIMAL(13, 4) NOT NULL,
-    vat DECIMAL(13, 4) NOT NULL,
+    sum_total CURRENCY NOT NULL,
+    vat CURRENCY NOT NULL,
     id_employee VARCHAR(10) NOT NULL,
-    card_number VARCHAR(13),
+    id_card VARCHAR(13),
     FOREIGN KEY (id_employee) REFERENCES Employee (id_employee) ON UPDATE CASCADE ON DELETE NO ACTION,
-    FOREIGN KEY (card_number) REFERENCES Customer_Card (card_number) ON UPDATE CASCADE ON DELETE NO ACTION
+    FOREIGN KEY (id_card) REFERENCES Customer_Card (id_card) ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
 CREATE TABLE IF NOT EXISTS Sale(
     UPC VARCHAR(12) NOT NULL,
-    check_number VARCHAR(10) NOT NULL,
-    product_number INT NOT NULL,
-    selling_price DECIMAL(13, 4) NOT NULL,
-    PRIMARY KEY (UPC, check_number),
+    id_check VARCHAR(10) NOT NULL,
+    product_number INTEGER NOT NULL,
+    selling_price CURRENCY NOT NULL,
+    PRIMARY KEY (UPC, id_check),
     FOREIGN KEY (UPC) REFERENCES Store_Product (UPC) ON UPDATE CASCADE ON DELETE NO ACTION,
-    FOREIGN KEY (check_number) REFERENCES Receipt (check_number) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (id_check) REFERENCES Receipt (id_check) ON UPDATE CASCADE ON DELETE CASCADE
 );
