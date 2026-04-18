34fcccb508a432819b5048d73383bd8b9d52e003
Perets Viktoria <vktrprc@gmail.com>
2026-04-18 19:50:14 +0300

    feat: add search and sorting for employees

--- server/src/routes/employeeRoutes.js
+++ server/src/routes/employeeRoutes.js
@@ -6,10 +6,12 @@ const router = Router();
 
 router.get('/me', verifyToken, employeeController.getMe);
 
-router.get('/', verifyToken, checkRole(['Менеджер']), employeeController.getAll);
 router.post('/', verifyToken, checkRole(['Менеджер']), employeeController.insertData);
 router.delete('/:id_employee', verifyToken, checkRole(['Менеджер']), employeeController.deleteEmployee);
 router.patch('/:id_employee', checkRole(['Менеджер']), employeeController.updateData);
 
-export default router;
+router.get("/", employeeController.getAllEmployees);
+router.get("/role/:empl_role", employeeController.getEmployeesByRole);
+router.get("/:empl_surname", employeeController.getEmployeeBySurname);
 
+export default router;
