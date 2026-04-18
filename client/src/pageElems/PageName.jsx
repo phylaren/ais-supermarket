c0d66120912ece8cb50d3dee1fedb40f219e4d50
phylaren <julboichuk@gmail.com>
2026-04-18 21:15:43 +0300

    feat: add exit button

--- client/src/pageElems/PageName.jsx
+++ client/src/pageElems/PageName.jsx
@@ -1,7 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import style from './PageName.module.css';
 
-export default function PageName({name}) {
+export default function PageName({ name }) {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('role');
+
+        navigate('/login'); 
+    };
+
     return (
-        <header className={style.header}>{name}</header>
-    )
+        <header className={style.header}>
+            <div className={style.title}>{name}</div>
+            
+            <button onClick={handleLogout} className={style.logoutButton} title="Вийти з системи">
+                Вихід 🚪
+            </button>
+        </header>
+    );
 }
